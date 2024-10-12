59e87b895252ed41a02fb17bad48ddb12c219ef6
niviten <l.j.nivethan@gmail.com>
2024-10-12 06:45:52 +0530

    zookeeper get children

--- internal/zookeeper/get_children.go
+++ internal/zookeeper/get_children.go
@@ -2,37 +2,88 @@ package zookeeper
 
 /*
 #cgo LDFLAGS: -L/usr/local/lib -lzookeeper_mt
+#include <string.h>
 #include <zookeeper/zookeeper.h>
 
-void get_children_cb(int rc, const struct String_vector* children, const void* data) {
+typedef struct {
+    int count;
+    char** children;
+    int done;
+} GetChildrenResult;
+
+GetChildrenResult* GetChildrenResult_create() {
+    GetChildrenResult* result = (GetChildrenResult*) malloc(sizeof(GetChildrenResult));
+    result->count = 0;
+    result->done = 0;
+    return result;
+}
+
+int GetChildrenResult_get_done(GetChildrenResult* result) {
+    return result->done;
+}
+
+int GetChildrenResult_get_count(GetChildrenResult* result) {
+    return result->count;
+}
+
+char* GetChildrenResult_get_child_at_index(GetChildrenResult* result, int index) {
+    return result->children[index];
+}
+
+void get_children_cb(int rc, const struct String_vector* children, void* data) {
     int i;
+    GetChildrenResult* result;
+
+    result = (GetChildrenResult*) data;
 
     if (rc == ZOK) {
-        printf("get children success\n");
+        result->count = children->count;
+        result->children = (char**) malloc(children->count * sizeof(char*));
         for (i = 0; i < children->count; i++) {
-            printf("c: %s\n", children->data[i]);
+            result->children[i] = (char*) malloc(strlen(children->data[i]) * sizeof(char));
+            strcpy(result->children[i], children->data[i]);
         }
+        result->done = 1;
     } else {
-        printf("get children failed\n");
+        result->done = -1;
     }
 }
 */
 import "C"
 import (
-	"fmt"
+	"errors"
 	"time"
 	"unsafe"
 )
 
-func (zk *ZooKeeper) GetChildren(path string) []string {
+func (zk *ZooKeeper) GetChildren(path string) ([]string, error) {
 	cPath := C.CString(path)
 	defer C.free(unsafe.Pointer(cPath))
 
-	r := C.zoo_aget_children(zk.zh, cPath, 0, (*[0]byte)(C.get_children_cb), nil)
+    result := C.GetChildrenResult_create()
 
-	fmt.Println("r: ", r)
+	r := C.zoo_aget_children(zk.zh, cPath, 0, (*[0]byte)(C.get_children_cb), unsafe.Pointer(result))
+    _ = r
 
-	time.Sleep(1 * time.Second)
+    for {
+        done := C.GetChildrenResult_get_done(result)
+        if done == 1 || done == -1 {
+            break
+        }
+        time.Sleep(51 * time.Millisecond)
+    }
 
-	return []string{}
+    done := C.GetChildrenResult_get_done(result)
+    if done == -1 {
+        return []string{}, errors.New("Zookeeper Get Children failed")
+    }
+
+    count := int(C.GetChildrenResult_get_count(result))
+
+    children := make([]string, count)
+    for i := 0; i < count; i++ {
+        children[i] = C.GoString(C.GetChildrenResult_get_child_at_index(result, C.int(i)))
+    }
+
+    return children, nil
 }
